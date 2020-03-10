6b777aec0429744d2c528694b330a960e89200fb
mming <szmingleo@gmail.com>
2020-03-10 08:58:28 +0800

    jd到家

--- quanx/Script/jddj.js.bak.js
+++ quanx/Script/jddj.js.bak.js
@@ -1,28 +1,44 @@
 const cookieName ='京东到家'
-const signurlKey = 'chen_signurl_jddj'
-const signheaderKey = 'chen_signheader_jddj'
+const cookieKey = 'chen_cookie_dj'
 const chen = init()
-const signurlVal = chen.getdata(signurlKey)
-const signheaderVal = chen.getdata(signheaderKey)
+
+const cookieVal = 'cookie自己抓'
 sign()
 function sign() {
-    let url = {url: signurlVal,headers: JSON.parse(signheaderVal)}
+    let url = {url: 'https://daojia.jd.com/client?functionId=signin%2FuserSigninNew&body=%7B%7D',
+    headers: { Cookie:cookieVal}}
+    url.headers['Connection'] = `keep-alive`
+    url.headers['Content-Type'] = `application/x-www-form-urlencoded;charset=UTF-8`
+    url.headers['Accept'] = `*/*`
+    url.headers['Host'] = `daojia.jd.com`
+    url.headers['User-Agent'] = `Mozilla/5.0 (iPhone; CPU iPhone OS 13_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 application=JDJR-App&deviceld=D6B42097-7660-464C-AE01-E5A3CFB2F788&clientType=ios`
+    url.headers['Accept-Language'] = `zn-ch`
+    url.headers['Accept-Encoding'] = `gzip, deflate, br`
+    url.headers['Referer'] = `https://daojia.jd.com/taroh5/h5dist/`
+   
     chen.get(url, (error, response, data) => {
       chen.log(`${cookieName}, data: ${data}`)
-      let res = JSON.parse(data)
+      let result = JSON.parse(data)
+      
       const title = `${cookieName}`
       let subTitle = ``
       let detail = ``
-      if (res.success&&res.result.points) {
-        subTitle = `签到结果:成功`
-        detail = `获取鲜豆：${res.result.points}`
-      } else if(!res.success){
+    
+      if (result.code == 0) {
+        subTitle = `签到结果:   成功`
+        detail = `获取鲜豆：${result.result.points}`
+      } else if(result.code==201){
+        subTitle = `签到结果: 失败`
+        detail = `说明: 未知`
+      } else {
         subTitle = `签到结果: 重复签到`
-        detail = `说明: ${res.msg}`
+        detail = `说明: ${result.msg}`
       }
       chen.msg(title, subTitle, detail)
+      chen.log(`${result.totalBalanceAmount}`)
     })
-  }
+    chen.done()
+    }
 
   function init() {
     isSurge = () => {
